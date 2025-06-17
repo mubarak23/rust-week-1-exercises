089f1e69ee2e5e4b750994758d9414fbfeb34ac5
mubarak23 <mubarakaminu340@gmail.com>
2025-06-17 12:20:04 +0100

    chore: fix format and clippy warnings

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,4 @@
-use std::io::{Cursor, Read};
+use std::io::Read;
 use std::num::ParseIntError;
 
 #[derive(Debug)]
@@ -14,7 +14,6 @@ impl From<std::io::Error> for Errors {
     }
 }
 
-
 impl From<ParseIntError> for Errors {
     fn from(err: ParseIntError) -> Self {
         Errors::HexError(err)
@@ -23,19 +22,15 @@ impl From<ParseIntError> for Errors {
 
 fn read_version_byte(transaction_bytes: &mut &[u8]) -> Result<u32, Errors> {
     let mut buffer = [0; 4];
-    transaction_bytes.read_exact(&mut buffer); // read exactly 4 bytes from transaction_bytes into the buffer stream;
+   let _ = transaction_bytes.read_exact(&mut buffer); // read exactly 4 bytes from transaction_bytes into the buffer stream;
     Ok(u32::from_le_bytes(buffer)) // converts a 4-byte array (buffer) into a 32-bit unsigned integer (u32) assuming little-endian byte order
 }
 
-
-
-
-
 // Implement extract_tx_version function below
 pub fn extract_tx_version(transaction_hex: &str) -> Result<u32, String> {
-    let mut transaction_bytes = match hex::decode(transaction_hex) {
+    let transaction_bytes = match hex::decode(transaction_hex) {
         Ok(bytes) => bytes,
-       Err(_) => return Err("Hex decode error".to_string()),
+        Err(_) => return Err("Hex decode error".to_string()),
     };
     if transaction_bytes.len() < 4 {
         return Err("Transaction data too short".to_string());
@@ -44,7 +39,7 @@ pub fn extract_tx_version(transaction_hex: &str) -> Result<u32, String> {
     let mut stream_bytes: &[u8] = &transaction_bytes;
     match read_version_byte(&mut stream_bytes) {
         Ok(version) => Ok(version),
-        Err(Errors::IoError (e)) => Err(format!("I/O error reading version: {}", e)),
+        Err(Errors::IoError(e)) => Err(format!("I/O error reading version: {}", e)),
         Err(Errors::TooShortError) => Err("Transaction data too short".to_string()),
         Err(Errors::HexError(_)) => Err("Unexpected hex error during version read".to_string()),
     }
