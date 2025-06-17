f1ee7bfea6458aeefa637eb028e935d01777af64
mubarak23 <mubarakaminu340@gmail.com>
2025-06-17 12:01:43 +0100

    chore:first implementation of extract_tx_version function

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,57 @@
+use std::io::{Cursor, Read};
+use std::num::ParseIntError;
+
+#[derive(Debug)]
+enum Errors {
+    IoError(std::io::Error),
+    HexError(ParseIntError),
+    TooShortError,
+}
+
+impl From<std::io::Error> for Errors {
+    fn from(err: std::io::Error) -> Self {
+        Error::Io(err)
+    }
+}
+
+impl From<std::io::Error> for Errors {
+    fn from(err: std::io::Error) -> Self {
+        Error::IoError(err)
+    }
+}
+
+
+impl From<ParseIntError> for Errors {
+    fn from(err: ParseIntError) -> Self {
+        Error::Hex(err)
+    }
+}
+
+fn read_version_byte(transaction_bytes: &[u8]) -> Result<u32, Errors> {
+    let mut buffer = [0; 4];
+    transaction_bytes.read_exact(&mut buffer); // read exactly 4 bytes from transaction_bytes into the buffer stream;
+    Ok(u32::from_le_bytes(buffer)) // converts a 4-byte array (buffer) into a 32-bit unsigned integer (u32) assuming little-endian byte order
+}
+
+
+
+
+
 // Implement extract_tx_version function below
 pub fn extract_tx_version(raw_tx_hex: &str) -> Result<u32, String> {
-    todo!()
+    let transaction_bytes = match hex::decode(transaction_hex) {
+        Ok(bytes) => bytes,
+        Err(_) => return Err("Hex decode error".to_string())
+    };
+    if transaction_bytes.len() < 4 {
+        return Err("Transaction data too short".to_string());
+    }
+    // let  stream_bytes = &transaction_bytes;
+    match read_version_byte(&transaction_bytes) {
+        Ok(version) => version,
+        Err(Errors::IoError (e)) => Err(format!("I/O error reading version: {}", e)),
+        Err(Error::TooShortError) => Err("Transaction data too short".to_string()),
+        Err(Error::HexError(_)) => Err("Unexpected hex error during version read".to_string()),
+    }
+    // todo!()
 }
