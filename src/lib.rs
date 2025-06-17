b89aa37b8d84054f483b14ed6051fe8276bea91f
mubarak23 <mubarakaminu340@gmail.com>
2025-06-17 12:13:29 +0100

    chore:fix error with variable reference in mut state

--- src/lib.rs
+++ src/lib.rs
@@ -10,24 +10,18 @@ enum Errors {
 
 impl From<std::io::Error> for Errors {
     fn from(err: std::io::Error) -> Self {
-        Error::Io(err)
-    }
-}
-
-impl From<std::io::Error> for Errors {
-    fn from(err: std::io::Error) -> Self {
-        Error::IoError(err)
+        Errors::IoError(err)
     }
 }
 
 
 impl From<ParseIntError> for Errors {
     fn from(err: ParseIntError) -> Self {
-        Error::Hex(err)
+        Errors::HexError(err)
     }
 }
 
-fn read_version_byte(transaction_bytes: &[u8]) -> Result<u32, Errors> {
+fn read_version_byte(transaction_bytes: &mut &[u8]) -> Result<u32, Errors> {
     let mut buffer = [0; 4];
     transaction_bytes.read_exact(&mut buffer); // read exactly 4 bytes from transaction_bytes into the buffer stream;
     Ok(u32::from_le_bytes(buffer)) // converts a 4-byte array (buffer) into a 32-bit unsigned integer (u32) assuming little-endian byte order
@@ -38,20 +32,21 @@ fn read_version_byte(transaction_bytes: &[u8]) -> Result<u32, Errors> {
 
 
 // Implement extract_tx_version function below
-pub fn extract_tx_version(raw_tx_hex: &str) -> Result<u32, String> {
-    let transaction_bytes = match hex::decode(transaction_hex) {
+pub fn extract_tx_version(transaction_hex: &str) -> Result<u32, String> {
+    let mut transaction_bytes = match hex::decode(transaction_hex) {
         Ok(bytes) => bytes,
-        Err(_) => return Err("Hex decode error".to_string())
+       Err(_) => return Err("Hex decode error".to_string()),
     };
     if transaction_bytes.len() < 4 {
         return Err("Transaction data too short".to_string());
     }
     // let  stream_bytes = &transaction_bytes;
-    match read_version_byte(&transaction_bytes) {
-        Ok(version) => version,
+    let mut stream_bytes: &[u8] = &transaction_bytes;
+    match read_version_byte(&mut stream_bytes) {
+        Ok(version) => Ok(version),
         Err(Errors::IoError (e)) => Err(format!("I/O error reading version: {}", e)),
-        Err(Error::TooShortError) => Err("Transaction data too short".to_string()),
-        Err(Error::HexError(_)) => Err("Unexpected hex error during version read".to_string()),
+        Err(Errors::TooShortError) => Err("Transaction data too short".to_string()),
+        Err(Errors::HexError(_)) => Err("Unexpected hex error during version read".to_string()),
     }
     // todo!()
 }
