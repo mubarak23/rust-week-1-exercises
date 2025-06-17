89f1649050f22c7bf998ff4f6bd5d8b0d673dc11
mubarak23 <mubarakaminu340@gmail.com>
2025-06-17 12:44:15 +0100

    chore:fix format space error

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,5 @@
 use std::io::{Error, Read};
 
-
 fn read_version_byte(transaction_bytes: &mut &[u8]) -> Result<u32, Error> {
     let mut buffer = [0; 4];
     let _ = transaction_bytes.read_exact(&mut buffer); // read exactly 4 bytes from transaction_bytes into the buffer stream;
@@ -13,14 +12,14 @@ pub fn extract_tx_version(transaction_hex: &str) -> Result<u32, String> {
         Ok(bytes) => bytes,
         Err(_) => return Err("Hex decode error".to_string()),
     };
+
     if transaction_bytes.len() < 4 {
         return Err("Transaction data too short".to_string());
     }
-    
+
     let mut stream_bytes: &[u8] = &transaction_bytes;
     match read_version_byte(&mut stream_bytes) {
         Ok(version) => Ok(version),
         Err(e) => Err(format!("{}", e)),
     }
-  
 }
